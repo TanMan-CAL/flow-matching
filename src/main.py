50662eb9006d9a5ff6a5113ec8b32aecf176191c
Tanmay Shah <70305211+TanMan-CAL@users.noreply.github.com>
2026-02-20 22:52:18 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,6 @@ import torch
 import os
 from diffusers import UNet2DConditionModel, DDIMScheduler
 from transformers import CLIPTextModel, CLIPTokenizer
-from diffusion import forward, iterative_denoise
-from utils import save_image
 
 from iterative_recursive_diffusion import *
 from utils.diffusion_utils import *
