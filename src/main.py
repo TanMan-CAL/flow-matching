09d7a7fa8cf39b7978f3b82e7452230c6bc89749
Tanmay Shah <70305211+TanMan-CAL@users.noreply.github.com>
2026-02-20 22:07:48 -0500

    .
    Removed unnecessary blank lines from the code.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,10 @@
 #src/main.py
-
 import torch
 import os
 from diffusers import UNet2DConditionModel, DDIMScheduler
 from transformers import CLIPTextModel, CLIPTokenizer
-
 from diffusion import forward, iterative_denoise
 from utils import save_image
-
-
 
 from iterative_recursive_diffusion import *
 from utils.diffusion_utils import *
