f7baa13f85874793636ca4818f4f6d298f47c871
Tanmay Shah <70305211+TanMan-CAL@users.noreply.github.com>
2026-03-26 00:33:29 -0400

    Update forward_process.py

--- model_techniques/forward_process.py
+++ model_techniques/forward_process.py
@@ -1,1 +1,5 @@
-
+def forward(image, t):
+    alpha_bar = alphas_cumprod[t]
+    noise = torch.randn_like(im) # sample random Gaussian noise
+    im_noisy = torch.sqrt(alpha_bar) * image + torch.sqrt(1 - alpha_bar) * noise
+    return image_noisy
