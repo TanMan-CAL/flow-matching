164c2c05c68cc20d67eb58b8081d5e7257da4d2d
Tanmay Shah <tanmayshah2007@gmail.com>
2026-04-12 00:48:25 -0400

    CFG methods

---
+++ diffusion-experimentation/classical_denoising.py
@@ -0,0 +1,2 @@
+# guassian blur: averages neighboring pixels, which does remove some noise but also destroys fine details and edges
+blurred = TF.gaussian_blur(noisy, kernel_size=5, sigma=3)
---
+++ diffusion-experimentation/iterative_recursive_diffusion.py
@@ -0,0 +1,3 @@
+#src/iterative_recursive_diffusion.py
+import torch
+from utils.diffusion_utils import *
