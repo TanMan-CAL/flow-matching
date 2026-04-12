a1051474cdad2fbeaad8a9780f3a50cccb12bc7d
Tanmay Shah <tanmayshah2007@gmail.com>
2026-04-12 00:47:07 -0400

    fix: added unet chunks and FCblock

--- src/main.py
+++ src/main.py
@@ -4,6 +4,3 @@ import numpy as np
 import os
 from diffusers import UNet2DConditionModel, DDIMScheduler
 from transformers import CLIPTextModel, CLIPTokenizer
-
-from iterative_recursive_diffusion import *
-from utils.diffusion_utils import *
